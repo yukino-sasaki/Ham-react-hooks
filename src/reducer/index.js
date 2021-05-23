76bb9eb45b386398f1ac05f359f709b5f31435dd
yukino-sasaki <yukisnowman.18@gmail.com>
2021-05-24 07:37:55 +0900

    操作ログを追加

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux'
 
 import events from './events'
+import operationLogs from './oprationLogs'
 
 console.log(combineReducers)
 //events.jsをimportして丸々conbinereducersで囲っている
-export default combineReducers({ events })
+export default combineReducers({ events, operationLogs })
---
+++ src/reducer/operationLogs.js
@@ -0,0 +1,4 @@
+import {
+    DELETE_OPARATION_LOGS,
+    ADD_OPARATION_LOG
+} from '../actions/index'
